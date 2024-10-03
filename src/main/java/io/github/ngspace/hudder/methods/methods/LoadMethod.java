2805cbf4467597f410a036c1aa1636ea6f7e50cd
ngspace <71827944+NGSpace@users.noreply.github.com>
2024-10-03 21:08:11 +0300

    Fix UI

--- src/main/java/io/github/ngspace/hudder/methods/methods/LoadMethod.java
+++ src/main/java/io/github/ngspace/hudder/methods/methods/LoadMethod.java
@@ -2,6 +2,7 @@ package io.github.ngspace.hudder.methods.methods;
 
 import java.io.IOException;
 
+import io.github.ngspace.hudder.Hudder;
 import io.github.ngspace.hudder.compilers.ATextCompiler;
 import io.github.ngspace.hudder.compilers.utils.CompileException;
 import io.github.ngspace.hudder.compilers.utils.CompileState;
@@ -18,7 +19,9 @@ public class LoadMethod implements IMethod {
 		try {
 			boolean AddText = (args.length<2 || args[1].asBoolean()) || type.equals("add");
 			ATextCompiler ecompiler=(args.length>2?Compilers.getCompilerFromName(args[2].asStringSafe()):comp);
+			for (var i : Hudder.precomplistners) i.accept(ecompiler);
 			meta.combineWithResult(ecompiler.compile(ci, HudFileUtils.getFile(args[0].asStringSafe())), AddText);
+			for (var i : Hudder.postcomplistners) i.accept(ecompiler);
 		} catch (ReflectiveOperationException | IllegalArgumentException | SecurityException | IOException e) {
 			throw new CompileException(e.getLocalizedMessage());
 		}
