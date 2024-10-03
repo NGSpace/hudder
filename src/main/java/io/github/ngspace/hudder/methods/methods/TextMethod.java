707b97f1b7b681cb49cfb97ad22ab85a071c021f
ngspace <71827944+NGSpace@users.noreply.github.com>
2024-10-03 22:11:19 +0300

    Fix text

--- src/main/java/io/github/ngspace/hudder/methods/methods/TextMethod.java
+++ src/main/java/io/github/ngspace/hudder/methods/methods/TextMethod.java
@@ -1,10 +1,8 @@
 package io.github.ngspace.hudder.methods.methods;
 
-import io.github.ngspace.hudder.Hudder;
 import io.github.ngspace.hudder.compilers.ATextCompiler;
 import io.github.ngspace.hudder.compilers.utils.CompileException;
 import io.github.ngspace.hudder.compilers.utils.CompileState;
-import io.github.ngspace.hudder.compilers.utils.Compilers;
 import io.github.ngspace.hudder.config.ConfigInfo;
 import io.github.ngspace.hudder.methods.MethodValue;
 import io.github.ngspace.hudder.methods.elements.TextElement;
@@ -16,20 +14,8 @@ public class TextMethod implements IMethod {
 		if (args.length<3) throw new CompileException(
 				"\""+type+"\" only accepts ;"+type+",[x],[y],[text],<scale>,<color>,<shadow>,<bg>,<bgcolor>;");
 		try {
-			ATextCompiler compiler = args.length>8 ? Compilers.getCompilerFromName(args[8].getAbsoluteValue()) 
-					: Compilers.getCompilerFromName("empty");
-			
-
-			int txlen = args[2].getAbsoluteValue().length();
-			String text = compiler.compile(ci, args[2].getAbsoluteValue().substring(txlen>1?1:0, txlen>1?txlen-1:0))
-					.TopLeftText;
+			String text = args[2].asString();
 			float scale = (float) (args.length>3 ? args[3].asDouble() : ci.scale);
-
-			int textwidth = Hudder.ins.textRenderer.getWidth(text);
-			compiler.put("text_width", textwidth);
-			compiler.put("string_width", textwidth);
-			compiler.put("strwidth", textwidth);
-			
 			
 			int x = args[0].asInt();
 			int y = args[1].asInt();
