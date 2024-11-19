632383b5c97fc80c39a2a980789c7719ff39f3d5
ngspace <71827944+NGSpace@users.noreply.github.com>
2024-11-19 19:41:56 +0200

    Create components map

---
+++ src/main/java/io/github/ngspace/hudder/data_management/ComponentsData.java
@@ -0,0 +1,9 @@
+package io.github.ngspace.hudder.data_management;
+
+import java.util.HashMap;
+
+public class ComponentsData extends HashMap<String, Object> {
+
+	private static final long serialVersionUID = 229002063971831208L;
+	
+}
