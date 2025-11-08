75e80e0c92b738b3a5afffb3f3a035c05972e827
ngspace <71827944+NGSpace@users.noreply.github.com>
2025-11-08 08:49:23 +0200

    Deprecate ObjectDataAPI fully

--- src/main/java/io/github/ngspace/hudder/data_management/ObjectDataAPI.java
+++ src/main/java/io/github/ngspace/hudder/data_management/ObjectDataAPI.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * @deprecated use DataVariableRegistry
+ */
 @Deprecated(since = "9.0.0", forRemoval = true)
 public class ObjectDataAPI {private ObjectDataAPI() {}
 	static List<Function<String, Object>> getters = new ArrayList<Function<String,Object>>();
