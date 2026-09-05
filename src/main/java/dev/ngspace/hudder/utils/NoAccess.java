664bff6e1d9e4175acfb276a91ee873fe79c5fa2
NGSpace <71827944+NGSpace@users.noreply.github.com>
2026-09-05 17:50:47 +0300

    fixes

--- src/main/java/dev/ngspace/hudder/utils/NoAccess.java
+++ src/main/java/dev/ngspace/hudder/utils/NoAccess.java
@@ -1,6 +1,7 @@
 package dev.ngspace.hudder.utils;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -11,4 +12,5 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
+@Inherited
 public @interface NoAccess {}
