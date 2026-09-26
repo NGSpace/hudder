394367c3be09e281954a122d1760fb3ccce85571
NGSpace <71827944+NGSpace@users.noreply.github.com>
2026-09-26 11:56:35 +0300

    Update to 26.4

--- src/main/java/dev/ngspace/hudder/main/HudderRenderer.java
+++ src/main/java/dev/ngspace/hudder/main/HudderRenderer.java
@@ -5,11 +5,11 @@ import java.util.function.Consumer;
 
 import org.joml.Matrix3x2fStack;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 import com.mojang.blaze3d.platform.NativeImage;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.renderpearl.api.pipeline.PrimitiveTopology;
-import com.mojang.renderpearl.api.pipeline.RenderPipeline;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.renderpearl.api.pipeline.PrimitiveTopology;
 
 import dev.ngspace.hudder.Hudder;
 import dev.ngspace.hudder.api.compilers.utils.HudInformation;
